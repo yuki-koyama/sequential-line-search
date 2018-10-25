d8aa905347b877b588f959415c48247ee10d1d8a
Yuki Koyama <yuki@koyama.xyz>
2018-10-25 09:13:19 +0900

    Improve readability and add some function descriptions

--- sequential_line_search/sliderutility.h
+++ sequential_line_search/sliderutility.h
@@ -7,10 +7,16 @@
 
 namespace SliderUtility
 {
-
-std::pair<Eigen::VectorXd, Eigen::VectorXd> enlargeSliderEnds(const Eigen::VectorXd& x_1, const Eigen::VectorXd& x_2, const double scale = 1.25, const double minimum_length = 1e-10);
-void mergeData(Eigen::MatrixXd& X, std::vector<Preference>& D, const double epsilon = 5e-03);
-
+    /// \brief Given a pair of slider ends, this function enlarges the slider by a specified scale
+    ///        but considering the bounding box constraint.
+    std::pair<Eigen::VectorXd, Eigen::VectorXd> enlargeSliderEnds(const Eigen::VectorXd& x_1,
+                                                                  const Eigen::VectorXd& x_2,
+                                                                  const double scale = 1.25,
+                                                                  const double minimum_length = 1e-10);
+    
+    /// \brief Merge sampled points that are sufficiently closer.
+    /// \param epsilon The threshold of the distance between sampled points to be merged
+    void mergeData(Eigen::MatrixXd& X, std::vector<Preference>& D, const double epsilon = 5e-03);
 }
 
 #endif // SLIDERUTILITY_H
--- sequential_line_search/utility.cpp
+++ sequential_line_search/utility.cpp
@@ -10,54 +10,52 @@ using Eigen::MatrixXd;
 
 namespace
 {
-std::random_device seed;
-std::default_random_engine gen(seed());
-std::uniform_real_distribution<double> uniform_dist(0.0, 1.0);
-std::normal_distribution<> normal_dist(0.0, 1.0);
+    std::random_device seed;
+    std::default_random_engine gen(seed());
+    std::uniform_real_distribution<double> uniform_dist(0.0, 1.0);
+    std::normal_distribution<> normal_dist(0.0, 1.0);
 }
 
 namespace Utility
 {
-
-Eigen::VectorXd generateRandomVector(unsigned n)
-{
-    Eigen::VectorXd x(n);
-    for (unsigned i = 0; i < n; ++ i)
+    Eigen::VectorXd generateRandomVector(unsigned n)
     {
-        x(i) = uniform_dist(gen);
-    }
-    return x;
-}
-
-double generateUniformReal()
-{
-    return uniform_dist(gen);
-}
-
-double generateStandardNormal()
-{
-    return normal_dist(gen);
-}
-
-void exportMatrixToCsv(const std::string& filePath, const Eigen::MatrixXd& X)
-{
-    std::ofstream ofs(filePath);
-    for (unsigned i = 0; i < X.rows(); ++ i)
-    {
-        for (unsigned j = 0; j < X.cols(); ++ j)
+        Eigen::VectorXd x(n);
+        for (unsigned i = 0; i < n; ++ i)
         {
-            ofs << X(i, j);
-            if (j + 1 != X.cols()) ofs << ",";
+            x(i) = uniform_dist(gen);
         }
-        ofs << std::endl;
+        return x;
     }
-}
-
-std::string getCurrentTimeInString()
-{
-    const std::time_t t = std::time(nullptr);
-    std::stringstream s; s << std::put_time(std::localtime(&t), "%Y%m%d%H%M%S");
-    return s.str();
-}
-
+    
+    double generateUniformReal()
+    {
+        return uniform_dist(gen);
+    }
+    
+    double generateStandardNormal()
+    {
+        return normal_dist(gen);
+    }
+    
+    void exportMatrixToCsv(const std::string& filePath, const Eigen::MatrixXd& X)
+    {
+        std::ofstream ofs(filePath);
+        for (unsigned i = 0; i < X.rows(); ++ i)
+        {
+            for (unsigned j = 0; j < X.cols(); ++ j)
+            {
+                ofs << X(i, j);
+                if (j + 1 != X.cols()) ofs << ",";
+            }
+            ofs << std::endl;
+        }
+    }
+    
+    std::string getCurrentTimeInString()
+    {
+        const std::time_t t = std::time(nullptr);
+        std::stringstream s; s << std::put_time(std::localtime(&t), "%Y%m%d%H%M%S");
+        return s.str();
+    }
 }
