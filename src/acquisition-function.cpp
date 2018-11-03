bbcfa02a4150ec1aa41762493a5d525108737ab5
Yuki Koyama <yuki@koyama.xyz>
2018-11-03 13:44:44 +0900

    Resolve meaningless nested namespaces

--- src/acquisition-function.cpp
+++ src/acquisition-function.cpp
@@ -10,16 +10,37 @@ using std::pair;
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
+namespace
+{
+    double objective(const std::vector<double> &x, std::vector<double>& /*grad*/, void* data)
+    {
+        const Regressor* regressor = static_cast<const Regressor*>(data);
+        return sequential_line_search::acquisition_function::CalculateAcqusitionValue(*regressor, Eigen::Map<const VectorXd>(&x[0], x.size()));
+    }
+    
+    // Schonlau et al, Global Versus Local Search in Constrained Optimization of Computer Models, 1997.
+    double objective_for_multiple_points(const std::vector<double> &x, std::vector<double>& /*grad*/, void* data)
+    {
+        const Regressor*                origRegressor    = static_cast<pair<const Regressor*, const GaussianProcessRegressor*>*>(data)->first;
+        const GaussianProcessRegressor* updatedRegressor = static_cast<pair<const Regressor*, const GaussianProcessRegressor*>*>(data)->second;
+        
+        const VectorXd _x = Eigen::Map<const VectorXd>(&x[0], x.size());
+        
+        const double y_best = origRegressor->gety().maxCoeff();
+        const double s_x    = updatedRegressor->estimate_s(_x);
+        const double u      = (origRegressor->estimate_y(_x) - y_best) / s_x;
+        const double Phi    = 0.5 * std::erf(u / std::sqrt(2.0)) + 0.5;
+        const double phi    = (1.0 / std::sqrt(2.0 * M_PI)) * std::exp(- u * u / 2.0);
+        const double EI     = s_x * (u * Phi + phi);
+        
+        return (s_x < 1e-08 || std::isnan(EI)) ? 0.0 : EI;
+    }
+}
+
 namespace sequential_line_search
 {
     namespace acquisition_function
     {
-        double objective(const std::vector<double> &x, std::vector<double>& /*grad*/, void* data)
-        {
-            const Regressor* regressor = static_cast<const Regressor*>(data);
-            return CalculateAcqusitionValue(*regressor, Eigen::Map<const VectorXd>(&x[0], x.size()));
-        }
-        
         double CalculateAcqusitionValue(const Regressor&       regressor,
                                         const Eigen::VectorXd& x,
                                         const FunctionType     function_type)
@@ -55,27 +76,6 @@ namespace sequential_line_search
             return x_star_local;
         }
         
-        ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-        // Schonlau et al, Global Versus Local Search in Constrained Optimization of Computer Models, 1997.
-        ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-        
-        double obj(const std::vector<double> &x, std::vector<double>& /*grad*/, void* data)
-        {
-            const Regressor*                origRegressor    = static_cast<pair<const Regressor*, const GaussianProcessRegressor*>*>(data)->first;
-            const GaussianProcessRegressor* updatedRegressor = static_cast<pair<const Regressor*, const GaussianProcessRegressor*>*>(data)->second;
-            
-            const VectorXd _x = Eigen::Map<const VectorXd>(&x[0], x.size());
-            
-            const double y_best = origRegressor->gety().maxCoeff();
-            const double s_x    = updatedRegressor->estimate_s(_x);
-            const double u      = (origRegressor->estimate_y(_x) - y_best) / s_x;
-            const double Phi    = 0.5 * std::erf(u / std::sqrt(2.0)) + 0.5;
-            const double phi    = (1.0 / std::sqrt(2.0 * M_PI)) * std::exp(- u * u / 2.0);
-            const double EI     = s_x * (u * Phi + phi);
-            
-            return (s_x < 1e-08 || std::isnan(EI)) ? 0.0 : EI;
-        }
-        
         vector<VectorXd> FindNextPoints(const Regressor&   regressor,
                                         const unsigned     n,
                                         const FunctionType function_type)
@@ -95,8 +95,8 @@ namespace sequential_line_search
             for (unsigned i = 0; i < n; ++ i)
             {
                 pair<const Regressor*, const GaussianProcessRegressor*> data(&regressor, &reg);
-                const VectorXd x_star_global = nloptutil::solve(x_ini, upper, lower, obj, nlopt::GN_DIRECT, &data, 800);
-                const VectorXd x_star_local  = nloptutil::solve(x_star_global, upper, lower, obj, nlopt::LN_COBYLA, &data, 200);
+                const VectorXd x_star_global = nloptutil::solve(x_ini, upper, lower, objective_for_multiple_points, nlopt::GN_DIRECT, &data, 800);
+                const VectorXd x_star_local  = nloptutil::solve(x_star_global, upper, lower, objective_for_multiple_points, nlopt::LN_COBYLA, &data, 200);
                 
                 points.push_back(x_star_local);
                 
