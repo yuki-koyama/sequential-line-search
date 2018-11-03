f026b1ca6da82990201914eda255d119e1f29bef
Yuki Koyama <yuki@koyama.xyz>
2018-11-04 08:25:33 +0900

    Improve comments

--- include/sequential-line-search/gaussianprocessregressor.h
+++ include/sequential-line-search/gaussianprocessregressor.h
@@ -2,40 +2,49 @@
 #define GAUSSIANPROCESSREGRESSOR_H
 
 #include <Eigen/Core>
-#include "regressor.h"
+#include <sequential-line-search/regressor.h>
 
 class GaussianProcessRegressor : public Regressor
 {
 public:
-    // Hyperparameters will be set via MAP estimation
+    /// \details Hyperparameters will be set via MAP estimation.
     GaussianProcessRegressor(const Eigen::MatrixXd &X, const Eigen::VectorXd &y);
-
-    // Specified hyperparameters will be used
+    
+    /// \details Specified hyperparameters will be used.
     GaussianProcessRegressor(const Eigen::MatrixXd &X, const Eigen::VectorXd &y, double a, double b, const Eigen::VectorXd& r);
-
+    
     double estimate_y(const Eigen::VectorXd& x) const override;
     double estimate_s(const Eigen::VectorXd& x) const override;
-
-    // Data
+    
+    /// \brief Data.
     Eigen::MatrixXd X;
+    
+    /// \brief Data.
     Eigen::VectorXd y;
-
-    // Derived from MAP or specified directly
+    
+    /// \brief A hyperparameter about signal level of ARD.
+    /// \details Derived from MAP or specified directly.
     double a;
+    
+    /// \brief A hyperparameter about noise level of ARD.
+    /// \details Derived from MAP or specified directly.
     double b;
+    
+    /// \brief A hyperparameter about length scales of ARD.
+    /// \details Derived from MAP or specified directly.
     Eigen::VectorXd r;
-
+    
     // Can be derived after MAP
     Eigen::MatrixXd C;
     Eigen::MatrixXd C_inv;
-
+    
     // Getter
     const Eigen::MatrixXd& getX() const override { return X; }
     const Eigen::VectorXd& gety() const override { return y; }
     double geta() const override { return a; }
     double getb() const override { return b; }
     const Eigen::VectorXd& getr() const override { return r; }
-
+    
 private:
     void compute_MAP();
 };
