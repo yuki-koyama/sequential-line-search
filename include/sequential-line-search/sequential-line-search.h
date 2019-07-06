1f6dc097cdb6c0fc9807b1d84af8a49fef1d1ae2
Yuki Koyama <yuki@koyama.xyz>
2019-07-06 15:21:44 +0900

    Add a wrapper class called SequentialLineSearchOptimizer

--- include/sequential-line-search/sequential-line-search.h
+++ include/sequential-line-search/sequential-line-search.h
@@ -1,12 +1,42 @@
 #ifndef sequential_line_search_h
 #define sequential_line_search_h
 
-#include <sequential-line-search/acquisition-function.h>
-#include <sequential-line-search/data.h>
-#include <sequential-line-search/gaussian-process-regressor.h>
-#include <sequential-line-search/preference-regressor.h>
-#include <sequential-line-search/preference.h>
-#include <sequential-line-search/slider.h>
-#include <sequential-line-search/utils.h>
+#include <Eigen/Core>
+#include <memory>
+#include <utility>
+
+namespace sequential_line_search
+{
+    class PreferenceRegressor;
+    class Slider;
+    class Data;
+
+    class SequentialLineSearchOptimizer
+    {
+    public:
+        SequentialLineSearchOptimizer(const int  dimension,
+                                      const bool use_slider_enlargement  = true,
+                                      const bool use_MAP_hyperparameters = true);
+
+        void setHyperparameters(
+            const double a, const double r, const double b, const double variance, const double btl_scale);
+
+        void submit(const double slider_position);
+
+        std::pair<Eigen::VectorXd, Eigen::VectorXd> getSliderEnds() const;
+        Eigen::VectorXd                             getParameters(const double slider_position) const;
+
+        Eigen::VectorXd getMaximizer() const;
+
+    private:
+        const int  m_dimension;
+        const bool m_use_slider_enlargement;
+        const bool m_use_MAP_hyperparameters;
+
+        std::shared_ptr<PreferenceRegressor> m_regressor;
+        std::shared_ptr<Slider>              m_slider;
+        std::shared_ptr<Data>                m_data;
+    };
+} // namespace sequential_line_search
 
 #endif // sequential_line_search_h
---
+++ src/sequential-line-search.cpp
@@ -0,0 +1,64 @@
+#include <sequential-line-search/acquisition-function.h>
+#include <sequential-line-search/data.h>
+#include <sequential-line-search/preference-regressor.h>
+#include <sequential-line-search/sequential-line-search.h>
+#include <sequential-line-search/slider.h>
+#include <sequential-line-search/utils.h>
+#include <stdexcept>
+
+sequential_line_search::SequentialLineSearchOptimizer::SequentialLineSearchOptimizer(const int  dimension,
+                                                                                     const bool use_slider_enlargement,
+                                                                                     const bool use_MAP_hyperparameters)
+    : m_dimension(dimension), m_use_slider_enlargement(use_slider_enlargement),
+      m_use_MAP_hyperparameters(use_MAP_hyperparameters)
+{
+    m_data      = std::make_shared<Data>();
+    m_regressor = nullptr;
+    m_slider    = std::make_shared<Slider>(
+        utils::generateRandomVector(m_dimension), utils::generateRandomVector(m_dimension), false);
+}
+
+void sequential_line_search::SequentialLineSearchOptimizer::setHyperparameters(
+    const double a, const double r, const double b, const double variance, const double btl_scale)
+{
+    PreferenceRegressor::Params::getInstance().a         = a;
+    PreferenceRegressor::Params::getInstance().r         = r;
+    PreferenceRegressor::Params::getInstance().b         = b;
+    PreferenceRegressor::Params::getInstance().variance  = variance;
+    PreferenceRegressor::Params::getInstance().btl_scale = btl_scale;
+}
+
+void sequential_line_search::SequentialLineSearchOptimizer::submit(const double slider_position)
+{
+    const auto x_chosen       = getParameters(slider_position);
+    const auto xs_slider_ends = getSliderEnds();
+
+    m_data->AddNewPoints(x_chosen, {xs_slider_ends.first, xs_slider_ends.second}, true);
+
+    m_regressor = std::make_shared<PreferenceRegressor>(m_data->X, m_data->D, m_use_MAP_hyperparameters);
+
+    const auto x_max = getMaximizer();
+    const auto x_ei  = acquisition_function::FindNextPoint(*m_regressor);
+
+    m_slider = std::make_shared<Slider>(x_max, x_ei, m_use_slider_enlargement);
+}
+
+std::pair<Eigen::VectorXd, Eigen::VectorXd> sequential_line_search::SequentialLineSearchOptimizer::getSliderEnds() const
+{
+    return {m_slider->end_0, m_slider->end_1};
+}
+
+Eigen::VectorXd sequential_line_search::SequentialLineSearchOptimizer::getParameters(const double slider_position) const
+{
+    return m_slider->getValue(slider_position);
+}
+
+Eigen::VectorXd sequential_line_search::SequentialLineSearchOptimizer::getMaximizer() const
+{
+    if (m_regressor == nullptr)
+    {
+        throw std::runtime_error("");
+    }
+
+    return m_regressor->find_arg_max();
+}
