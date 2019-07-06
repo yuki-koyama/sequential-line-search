9f581a921b5f41d0336ebe930cefcb642cf717ed
Yuki Koyama <yuki@koyama.xyz>
2019-07-06 14:05:45 +0900

    Get rid of some namespace nests

--- src/utils.cpp
+++ src/utils.cpp
@@ -5,20 +5,14 @@
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-namespace sequential_line_search
+Eigen::VectorXd sequential_line_search::utils::generateRandomVector(unsigned n)
 {
-    namespace utils
-    {
-        Eigen::VectorXd generateRandomVector(unsigned n)
-        {
-            return 0.5 * (Eigen::VectorXd::Random(n) + Eigen::VectorXd::Ones(n));
-        }
+    return 0.5 * (Eigen::VectorXd::Random(n) + Eigen::VectorXd::Ones(n));
+}
 
-        void exportMatrixToCsv(const std::string& filePath, const Eigen::MatrixXd& X)
-        {
-            std::ofstream   file(filePath);
-            Eigen::IOFormat format(Eigen::StreamPrecision, Eigen::DontAlignCols, ",");
-            file << X.format(format);
-        }
-    } // namespace utils
-} // namespace sequential_line_search
+void sequential_line_search::utils::exportMatrixToCsv(const std::string& filePath, const Eigen::MatrixXd& X)
+{
+    std::ofstream   file(filePath);
+    Eigen::IOFormat format(Eigen::StreamPrecision, Eigen::DontAlignCols, ",");
+    file << X.format(format);
+}
