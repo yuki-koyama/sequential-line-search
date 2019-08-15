80cf5caf2e87f16b48dcf512a5b719530a2ac08d
Yuki Koyama <yuki@koyama.xyz>
2019-08-15 23:12:32 +0900

    Remove unused functions

--- include/sequential-line-search/utils.h
+++ include/sequential-line-search/utils.h
@@ -19,53 +19,6 @@ namespace sequential_line_search
         Eigen::VectorXd generateRandomVector(unsigned n);
 
         ////////////////////////////////////////////////
-        // Math
-        ////////////////////////////////////////////////
-
-        inline double derivative_sigmoid(double d)
-        {
-            const double exp_d     = std::exp(d);
-            const double exp_d_one = exp_d + 1.0;
-            return -exp_d / (exp_d_one * exp_d_one);
-        }
-
-        inline double sigmoid(double d) { return 1.0 / (1.0 + std::exp(-d)); }
-
-        inline double gauss(const Eigen::VectorXd& x,
-                            const Eigen::VectorXd& mu,
-                            const Eigen::MatrixXd& Sigma_inv,
-                            const double           Sigma_det)
-        {
-            const unsigned n = x.rows();
-            return (1.0 / (pow(2.0 * M_PI, 0.5 * n) * std::sqrt(Sigma_det))) *
-                   std::exp(-0.5 * (x - mu).transpose() * Sigma_inv * (x - mu));
-        }
-
-        inline double gauss(const Eigen::VectorXd& x, const Eigen::VectorXd& mu, const Eigen::MatrixXd& Sigma)
-        {
-            const unsigned n = x.rows();
-            return (1.0 / (pow(2.0 * M_PI, 0.5 * n) * std::sqrt(Sigma.determinant()))) *
-                   std::exp(-0.5 * (x - mu).transpose() * Sigma.inverse() * (x - mu));
-        }
-
-        ////////////////////////////////////////////////
-        // Bradley-Terry Model
-        ////////////////////////////////////////////////
-
-        // Probability that f_1 > f_2 (f_1 is preferable to f_2)
-        inline double BT(double f_1, double f_2, double scale = 1.0) { return sigmoid((f_1 - f_2) / scale); }
-
-        inline double derivative_BT_f_1(double f_1, double f_2, double scale = 1.0)
-        {
-            return (1.0 / scale) * derivative_sigmoid((f_1 - f_2) / scale);
-        }
-
-        inline double derivative_BT_f_2(double f_1, double f_2, double scale = 1.0)
-        {
-            return -(1.0 / scale) * derivative_sigmoid((f_1 - f_2) / scale);
-        }
-
-        ////////////////////////////////////////////////
         // Bradley-Terry-Luce Model
         ////////////////////////////////////////////////
 
