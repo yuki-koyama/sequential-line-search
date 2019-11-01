de0109dce6679b5f859c2bedaf925c8f4905db7f
Yuki Koyama <yuki@koyama.xyz>
2019-11-01 19:51:59 +0900

    Unnest namespace

--- src/preference-regressor.cpp
+++ src/preference-regressor.cpp
@@ -245,155 +245,151 @@ namespace
     }
 } // namespace
 
-namespace sequential_line_search
+sequential_line_search::PreferenceRegressor::PreferenceRegressor(const MatrixXd&                X,
+                                                                 const std::vector<Preference>& D,
+                                                                 bool                           use_map_hyperparameters,
+                                                                 const double                   default_a,
+                                                                 const double                   default_r,
+                                                                 const double                   default_b,
+                                                                 const double                   variance,
+                                                                 const double                   btl_scale)
+    : m_use_map_hyperparameters(use_map_hyperparameters),
+      X(X),
+      D(D),
+      m_default_a(default_a),
+      m_default_r(default_r),
+      m_default_b(default_b),
+      m_variance(variance),
+      m_btl_scale(btl_scale)
 {
-    PreferenceRegressor::PreferenceRegressor(const MatrixXd&                X,
-                                             const std::vector<Preference>& D,
-                                             bool                           use_map_hyperparameters,
-                                             const double                   default_a,
-                                             const double                   default_r,
-                                             const double                   default_b,
-                                             const double                   variance,
-                                             const double                   btl_scale)
-        : m_use_map_hyperparameters(use_map_hyperparameters),
-          X(X),
-          D(D),
-          m_default_a(default_a),
-          m_default_r(default_r),
-          m_default_b(default_b),
-          m_variance(variance),
-          m_btl_scale(btl_scale)
+    if (X.cols() == 0 || D.size() == 0)
     {
-        if (X.cols() == 0 || D.size() == 0)
-        {
-            return;
-        }
-
-        PerformMapEstimation();
-
-        C     = CalcLargeKY(X, Concat(a, r), b);
-        C_inv = C.inverse();
+        return;
     }
 
-    double PreferenceRegressor::PredictMu(const VectorXd& x) const
-    {
-        const VectorXd k = CalcSmallK(x, X, Concat(a, r));
-        return k.transpose() * C_inv * y;
-    }
+    PerformMapEstimation();
 
-    double PreferenceRegressor::PredictSigma(const VectorXd& x) const
-    {
-        const VectorXd k = CalcSmallK(x, X, Concat(a, r));
-        return std::sqrt(a - k.transpose() * C_inv * k);
-    }
+    C     = CalcLargeKY(X, Concat(a, r), b);
+    C_inv = C.inverse();
+}
 
-    Eigen::VectorXd PreferenceRegressor::PredictMuDerivative(const Eigen::VectorXd& x) const
-    {
-        // TODO: Incorporate a mean function
-        const MatrixXd k_x_derivative = CalcSmallKSmallXDerivative(x, X, Concat(a, r));
-        return k_x_derivative * C_inv * y;
-    }
+double sequential_line_search::PreferenceRegressor::PredictMu(const VectorXd& x) const
+{
+    const VectorXd k = CalcSmallK(x, X, Concat(a, r));
+    return k.transpose() * C_inv * y;
+}
 
-    Eigen::VectorXd PreferenceRegressor::PredictSigmaDerivative(const Eigen::VectorXd& x) const
-    {
-        const MatrixXd k_x_derivative = CalcSmallKSmallXDerivative(x, X, Concat(a, r));
-        const VectorXd k              = CalcSmallK(x, X, Concat(a, r));
-        const double   sigma          = PredictSigma(x);
-        return -(1.0 / sigma) * k_x_derivative * C_inv * k;
-    }
+double sequential_line_search::PreferenceRegressor::PredictSigma(const VectorXd& x) const
+{
+    const VectorXd k = CalcSmallK(x, X, Concat(a, r));
+    return std::sqrt(a - k.transpose() * C_inv * k);
+}
 
-    void PreferenceRegressor::PerformMapEstimation(const PreferenceRegressor* previous_iter_regressor)
-    {
-        const unsigned M = X.cols();
-        const unsigned d = X.rows();
+VectorXd sequential_line_search::PreferenceRegressor::PredictMuDerivative(const VectorXd& x) const
+{
+    // TODO: Incorporate a mean function
+    const MatrixXd k_x_derivative = CalcSmallKSmallXDerivative(x, X, Concat(a, r));
+    return k_x_derivative * C_inv * y;
+}
 
-        VectorXd upper              = VectorXd::Constant(M + 2 + d, +1e+01);
-        VectorXd lower              = VectorXd::Constant(M + 2 + d, -1e+01);
-        lower.block(M, 0, 2 + d, 1) = VectorXd::Constant(2 + d, 1e-05);
-        VectorXd x_ini              = VectorXd::Constant(M + 2 + d, 0.0);
-        x_ini(M + 0)                = m_default_a;
+VectorXd sequential_line_search::PreferenceRegressor::PredictSigmaDerivative(const VectorXd& x) const
+{
+    const MatrixXd k_x_derivative = CalcSmallKSmallXDerivative(x, X, Concat(a, r));
+    const VectorXd k              = CalcSmallK(x, X, Concat(a, r));
+    const double   sigma          = PredictSigma(x);
+    return -(1.0 / sigma) * k_x_derivative * C_inv * k;
+}
+
+void sequential_line_search::PreferenceRegressor::PerformMapEstimation(
+    const PreferenceRegressor* previous_iter_regressor)
+{
+    const unsigned M = X.cols();
+    const unsigned d = X.rows();
+
+    VectorXd upper              = VectorXd::Constant(M + 2 + d, +1e+01);
+    VectorXd lower              = VectorXd::Constant(M + 2 + d, -1e+01);
+    lower.block(M, 0, 2 + d, 1) = VectorXd::Constant(2 + d, 1e-05);
+    VectorXd x_ini              = VectorXd::Constant(M + 2 + d, 0.0);
+    x_ini(M + 0)                = m_default_a;
 #ifdef SEQUENTIAL_LINE_SEARCH_USE_NOISELESS_FORMULATION
-        x_ini(M + 1) = 0.5 * (upper(M + 1) + lower(M + 1));
+    x_ini(M + 1) = 0.5 * (upper(M + 1) + lower(M + 1));
 #else
-        x_ini(M + 1) = m_default_b;
+    x_ini(M + 1) = m_default_b;
 #endif
-        x_ini.block(M + 2, 0, d, 1) = VectorXd::Constant(d, m_default_r);
+    x_ini.block(M + 2, 0, d, 1) = VectorXd::Constant(d, m_default_r);
 
-        // Use the MAP estimated values in previous regression as initial values
-        if (previous_iter_regressor != nullptr)
+    // Use the MAP estimated values in previous regression as initial values
+    if (previous_iter_regressor != nullptr)
+    {
+        for (unsigned i = 0; i < M; ++i)
         {
-            for (unsigned i = 0; i < M; ++i)
-            {
-                x_ini(i) = previous_iter_regressor->PredictMu(X.col(i));
-            }
-            x_ini(M + 0)                = previous_iter_regressor->a;
-            x_ini(M + 1)                = previous_iter_regressor->b;
-            x_ini.block(M + 2, 0, d, 1) = previous_iter_regressor->r;
+            x_ini(i) = previous_iter_regressor->PredictMu(X.col(i));
         }
+        x_ini(M + 0)                = previous_iter_regressor->a;
+        x_ini(M + 1)                = previous_iter_regressor->b;
+        x_ini.block(M + 2, 0, d, 1) = previous_iter_regressor->r;
+    }
 
 #ifdef VERBOSE
-        timer::Timer t("PreferenceRegressor::compute_MAP");
+    timer::Timer t("PreferenceRegressor::compute_MAP");
 #endif
 
-        const VectorXd x_opt = nloptutil::solve(x_ini, upper, lower, objective, nlopt::LD_TNEWTON, this, true, 100);
+    const VectorXd x_opt = nloptutil::solve(x_ini, upper, lower, objective, nlopt::LD_TNEWTON, this, true, 100);
 
-        y = x_opt.segment(0, M);
+    y = x_opt.segment(0, M);
 
 #ifdef VERBOSE
-        std::cout << "Estimated values: " << y.transpose().format(Eigen::IOFormat(3)) << std::endl;
+    std::cout << "Estimated values: " << y.transpose().format(Eigen::IOFormat(3)) << std::endl;
 #endif
 
-        if (m_use_map_hyperparameters)
-        {
-            a = x_opt(M + 0);
+    if (m_use_map_hyperparameters)
+    {
+        a = x_opt(M + 0);
 #ifdef SEQUENTIAL_LINE_SEARCH_USE_NOISELESS_FORMULATION
-            b = b_fixed;
+        b = b_fixed;
 #else
-            b = x_opt(M + 1);
+        b = x_opt(M + 1);
 #endif
-            r = x_opt.block(M + 2, 0, d, 1);
+        r = x_opt.block(M + 2, 0, d, 1);
 
 #ifdef VERBOSE
-            std::cout << "Learned hyperparameters ... a: " << a << ", \tb: " << b << ", \tr: " << r.transpose()
-                      << std::endl;
+        std::cout << "Learned hyperparameters ... a: " << a << ", \tb: " << b << ", \tr: " << r.transpose()
+                  << std::endl;
 #endif
-        }
-        else
-        {
-            a = m_default_a;
-            b = m_default_b;
-            r = VectorXd::Constant(d, m_default_r);
-        }
     }
-
-    ///////////////////////////////////////////////////////////////////
-
-    VectorXd PreferenceRegressor::FindArgMax() const
+    else
     {
-        int i;
-        y.maxCoeff(&i);
-        return X.col(i);
+        a = m_default_a;
+        b = m_default_b;
+        r = VectorXd::Constant(d, m_default_r);
     }
+}
 
-    void PreferenceRegressor::DampData(const std::string& dir_path, const std::string& prefix) const
+VectorXd sequential_line_search::PreferenceRegressor::FindArgMax() const
+{
+    int i;
+    y.maxCoeff(&i);
+    return X.col(i);
+}
+
+void sequential_line_search::PreferenceRegressor::DampData(const std::string& dir_path, const std::string& prefix) const
+{
+    // Export X using CSV
+    utils::ExportMatrixToCsv(dir_path + "/" + prefix + "X.csv", X);
+
+    // Export D using CSV
+    std::ofstream ofs_D(dir_path + "/" + prefix + "D.csv");
+    for (unsigned i = 0; i < D.size(); ++i)
     {
-        // Export X using CSV
-        utils::ExportMatrixToCsv(dir_path + "/" + prefix + "X.csv", X);
-
-        // Export D using CSV
-        std::ofstream ofs_D(dir_path + "/" + prefix + "D.csv");
-        for (unsigned i = 0; i < D.size(); ++i)
+        for (unsigned j = 0; j < D[i].size(); ++j)
         {
-            for (unsigned j = 0; j < D[i].size(); ++j)
+            ofs_D << D[i][j];
+
+            if (j + 1 != D[i].size())
             {
-                ofs_D << D[i][j];
-
-                if (j + 1 != D[i].size())
-                {
-                    ofs_D << ",";
-                }
+                ofs_D << ",";
             }
-            ofs_D << std::endl;
         }
+        ofs_D << std::endl;
     }
-} // namespace sequential_line_search
+}
