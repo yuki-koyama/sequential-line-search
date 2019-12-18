34e2af22b72b4e3ad1eb76e564d316d30d04bec3
Yuki Koyama <yuki@koyama.xyz>
2019-12-18 23:23:49 +0900

    Fix python bindings by adding missing headers

--- python/pySequentialLineSearch.cpp
+++ python/pySequentialLineSearch.cpp
@@ -1,5 +1,7 @@
 #include <pybind11/eigen.h>
+#include <pybind11/functional.h>
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 #include <sequential-line-search/sequential-line-search.hpp>
 
 using sequential_line_search::SequentialLineSearchOptimizer;
