8882b891a703e953dabbd91ac06a2cf9c17a5e38
Yuki Koyama <yuki@koyama.xyz>
2020-02-25 22:51:29 +0900

    Update the python API but with backward compatibility

--- python/pySequentialLineSearch.cpp
+++ python/pySequentialLineSearch.cpp
@@ -37,12 +37,6 @@ PYBIND11_MODULE(pySequentialLineSearch, m)
 
     py::class_<SequentialLineSearchOptimizer> optimizer_class(m, "SequentialLineSearchOptimizer");
 
-    optimizer_class.def(py::init<const int, const bool, const bool, const sequential_line_search::KernelType>(),
-                        "num_dims"_a,
-                        "use_slider_enlargement"_a = true,
-                        "use_map_hyperparams"_a    = true,
-                        "kernel_type"_a            = sequential_line_search::KernelType::ArdMatern52Kernel);
-
     optimizer_class.def(py::init<const int,
                                  const bool,
                                  const bool,
@@ -51,11 +45,11 @@ PYBIND11_MODULE(pySequentialLineSearch, m)
                                  const std::function<std::pair<Eigen::VectorXd, Eigen::VectorXd>(const int)>&,
                                  const sequential_line_search::SliderEndSelectionStrategy>(),
                         "num_dims"_a,
-                        "use_slider_enlargement"_a = true,
-                        "use_map_hyperparams"_a    = true,
-                        "kernel_type"_a            = sequential_line_search::KernelType::ArdMatern52Kernel,
-                        "acquisition_func_type"_a  = sequential_line_search::AcquisitionFuncType::ExpectedImprovement,
-                        "initial_slider_generator"_a,
+                        "use_slider_enlargement"_a   = true,
+                        "use_map_hyperparams"_a      = true,
+                        "kernel_type"_a              = sequential_line_search::KernelType::ArdMatern52Kernel,
+                        "acquisition_func_type"_a    = sequential_line_search::AcquisitionFuncType::ExpectedImprovement,
+                        "initial_slider_generator"_a = sequential_line_search::GenerateRandomSliderEnds,
                         "slider_end_selection_strategy"_a =
                             sequential_line_search::SliderEndSelectionStrategy::LargestExpectValue);
 
