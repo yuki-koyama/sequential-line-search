2d8479b0f6bf984612dcd8376a3900c3144967df
Yuki Koyama <yuki@koyama.xyz>
2020-02-25 21:50:54 +0900

    Improve comments

--- include/sequential-line-search/slider.hpp
+++ include/sequential-line-search/slider.hpp
@@ -9,7 +9,9 @@ namespace sequential_line_search
     {
     public:
         /// \param end_0 The first end-point, which is expected to be x^{+} in [Koyama+17]
+        ///
         /// \param end_1 The second end-point, which is expected to be x^{EI} in [Koyama+17]
+        ///
         /// \param enlarge When this is true, the slider enlargement post-processing will be performed.
         Slider(const Eigen::VectorXd& end_0,
                const Eigen::VectorXd& end_1,
@@ -19,10 +21,16 @@ namespace sequential_line_search
 
         Eigen::VectorXd GetValue(const double t) const { return (1.0 - t) * end_0 + t * end_1; }
 
+        /// When enlargement is enabled, this is not the same as x^{+} (or the current best point)
         Eigen::VectorXd end_0;
+
+        /// When enlargement is enabled, this is not the same as x^{EI} (or the Bayesian optimization choice)
         Eigen::VectorXd end_1;
 
+        /// x^{+} (or the current best point)
         Eigen::VectorXd original_end_0;
+
+        /// x^{EI} (or the Bayesian optimization choice)
         Eigen::VectorXd original_end_1;
     };
 } // namespace sequential_line_search
