80fb73694b57c65d5e93b22a93e4d4e2909af3ab
Yuki Koyama <yuki@koyama.xyz>
2023-03-25 22:34:38 +0900

    Increment the python version

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name="pySequentialLineSearch",
-    version="0.3",
+    version="0.4",
     author="Yuki Koyama",
     author_email="yuki@koyama.xyz",
     description="Python bindings of sequential line search",
